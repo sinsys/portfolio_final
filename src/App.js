f5f1f71e77c37d302e79d1a1909d62cc90183beb
sinsys <nicholas.hazel@gmail.com>
2020-06-27 17:43:30 -0700

    Attempting full screen app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 // Root - App
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Router from 'routers/Router';
 import MenuBar from 'components/widgets/MenuBar';
+
+import useFullscreenStatus from 'hooks/fullscreen';
 
 // Files / Assets
 import './App.scss';
 
 function App() {
+
+  const maximizableElement = useRef(null);
+  let [isFullscreen, setIsFullscreen] = useState(false);
+  let errorMessage;
+  
+  try {
+    [isFullscreen, setIsFullscreen] = useFullscreenStatus(maximizableElement);
+  } catch (e) {
+    errorMessage = "Fullscreen not supported";
+    isFullscreen = false;
+    setIsFullscreen = undefined;
+  }
+
   return (
-    <div className="App">
+    <div className="App" ref={maximizableElement}>
       <MenuBar />
       <Router />
     </div>
